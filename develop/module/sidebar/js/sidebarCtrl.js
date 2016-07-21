051a9cb3bd71095b8e4850afc8188e4fb588fd6b
马若涵 <marh@zhong-ying.com>
2016-07-21 18:35:21 +0800

    update!

--- develop/module/sidebar/js/sidebarCtrl.js
+++ develop/module/sidebar/js/sidebarCtrl.js
@@ -2,7 +2,7 @@
 	angular.module('App').controller('SidebarCtrl',['$scope','$location',function($scope,$location){
 			console.info($location);
 		$scope.isActive=function(path){
-			return {'active':true}
-		}
-	}])
+			return {'active':path == $location.$$path ? true : false};
+		};
+	}]);
 })();
